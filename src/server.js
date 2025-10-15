45884d1fbd466558cd3e25221c3d9bf700654af2
billtai <anhtaipxuan@gmail.com>
2025-10-15 09:50:31 +0700

    Init Code

---
+++ src/server.js
@@ -0,0 +1,15 @@
+// const express = require('express')
+import express from 'express'
+
+const app = express()
+
+const hostname = 'localhost'
+const port = 8017
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World Nodejs Anh Tai</h1>')
+})
+
+app.listen(port, hostname, () => {
+  console.log(`Hello Anh Tai, I'am running server at http://${hostname}:${port}/`)
+})
