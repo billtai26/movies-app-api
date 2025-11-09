f6abfe1b55aff01ac1b3856a37365883c4e4ac38
billtai <anhtaipxuan@gmail.com>
2025-11-09 16:07:53 +0700

    Hoàn thiện API quản lý bình luận

--- src/routes/v1/reviewRoute.js
+++ src/routes/v1/reviewRoute.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import { reviewController } from '~/controllers/reviewController'
 import { protect } from '~/middlewares/authMiddleware'
-// import { admin } from '~/middlewares/adminMiddleware' // (Dùng nếu bạn muốn admin xóa/sửa review)
+import { admin } from '~/middlewares/adminMiddleware'
+// import { reviewValidation } from '~/validations/reviewValidation'
 
 const Router = express.Router()
 
@@ -25,5 +26,23 @@ Router.route('/movie/:movieId')
 Router.route('/:id')
   .put(protect, reviewController.updateReview)
   .delete(protect, reviewController.deleteReview)
+
+// === Admin Routes (Mới) ===
+// GET /v1/reviews/admin/list (Lọc, Phân trang)
+Router.route('/admin/list')
+  .get(protect, admin, reviewController.adminGetReviews)
+
+// GET /v1/reviews/admin/:id (Xem chi tiết)
+// PATCH /v1/reviews/admin/:id (Sửa)
+// DELETE /v1/reviews/admin/:id (Xoá)
+Router.route('/admin/:id')
+  .get(protect, admin, reviewController.adminGetReviewDetails)
+  // .patch(
+  //   protect,
+  //   admin,
+  //   reviewValidation.adminUpdate,
+  //   reviewController.adminUpdateReview
+  // )
+  .delete(protect, admin, reviewController.adminDeleteReview)
 
 export const reviewRoute = Router
