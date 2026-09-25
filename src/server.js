9f89152faa0254c844d640593535a3b5044cb533
anhtai26 <anhtaipxuan@gmail.com>
2026-09-25 11:49:51 +0700

    Fix: Fix lỗi Cannot Get / khi deploy

--- src/server.js
+++ src/server.js
@@ -100,6 +100,13 @@ const START_SERVER = () => {
   app.use(passport.initialize())
   configurePassport() // Gọi hàm config
 
+  app.get('/', (req, res) => {
+    res.status(200).json({
+      message: 'Movies App Backend API is running successfully!',
+      docs: '/v1'
+    })
+  })
+
   // Sử dụng Routers
   app.use('/v1', APIs_V1)
 
